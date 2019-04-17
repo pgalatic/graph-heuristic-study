5c7c4a0514a71a2ed4bf5443a1cfa1cb1cd943cf
Ari Zerner <ariel@zerner.com>
2019-04-17 19:39:16 -0400

    Separated non-DE specific logic from DE

--- driver.py
+++ driver.py
@@ -17,7 +17,7 @@ import networkx as nx
 import tabulate as tab
 
 # PROJECT LIB
-import de
+import de as de
 # import pso
 # import aco
 from greedy_color import greedy_color
@@ -41,23 +41,23 @@ def log(s):
 def parse_args():
     '''Parses arguments'''
     ap = argparse.ArgumentParser()
-    
+
     ap.add_argument('mode', type=str,
         help='[s|m|l] -- small, medium, or large')
     ap.add_argument('n', type=int,
         help='the number of graphs to analyze')
-    
+
     return ap.parse_args()
 
 def gen_rand_graphs(dim, n, theta=0.5):
     '''Generates random graphs based on parameter theta'''
     graphs = []
-    
+
     log(f'generating {n} Erdős-Rényi graphs of size {dim}...')
     for _ in range(n):
         # create a blank adjacency matrix
         graph = np.zeros(shape=(dim, dim))
-        
+
         # add edges according to a random process
         for idx in range(dim - 2):
             for idy in range(idx + 1, dim - 1):
@@ -67,21 +67,21 @@ def gen_rand_graphs(dim, n, theta=0.5):
                     graph[idx - 1][idy - 1] = 1
                     graph[idy - 1][idx - 1] = 1
         graphs.append(graph)
-    
+
     return [nx.from_numpy_array(graph) for graph in graphs]
-    
+
 def record_graphs(graphs, path):
-    '''writes graphs in g6 format to the designated filename'''    
+    '''writes graphs in g6 format to the designated filename'''
     for graph in graphs:
         with open('temp', 'w') as temp:
             nx.write_graph6(graph, 'temp', header=False)
         with open('temp', 'r') as temp:
             with open(path, 'a') as out:
                 out.write(''.join(temp.readlines()))
-    
+
     with open(path, 'r') as f:
         log('Recorded %d graphs' % len(f.readlines()))
-    
+
     os.remove('temp')
 
 def load_graphs(mode, num):
@@ -90,7 +90,7 @@ def load_graphs(mode, num):
     the graph to its chromatic number.
     '''
     log('loading graphs...')
-    
+
     if 's' in mode:
         fname = str(GRAPH_DIR / SML_NAME)
     elif 'm' in mode:
@@ -99,27 +99,37 @@ def load_graphs(mode, num):
         fname = str(GRAPH_DIR / LRG_NAME)
     else:
         raise Exception(f'Could not recognize mode: {mode}.')
-    
+
     with open(fname, 'r', newline='\n', encoding='ISO-8859-1') as f:
         length = sum(1 for _ in f)
-    
+
     if num >= length:
         num = length - 1
         log(f'WARNING: Can only load up to {length} graphs from this file.')
     idxs = random.sample(range(length), int(num))
     raw_graphs = [linecache.getline(fname, idx) for idx in idxs]
-    
+
     # create temporary file so that nx can read it
     temp_path = 'temp'
     with open(temp_path, 'w', newline='\n') as f:
         f.writelines(raw_graphs)
-    
+
     graphs = nx.read_graph6(temp_path)
     os.remove(temp_path)
-    
+
     log(f'...{len(graphs)} graphs loaded.')
-    
+
     return graphs
+
+def compute_chi(optim_module, graphs):
+    '''
+    Computes the set of best found chi numbers for a given graph
+    '''
+    results = dict()
+    for graph in graphs:
+        gbest = optim_module.Optimizer(graph=graph).run()
+        results[graph] = greedy_color(graph, gbest.get_order(gbest.position))
+    return results
 
 def main():
     '''Driver program'''
@@ -133,15 +143,15 @@ def main():
         record_graphs(gen_rand_graphs(MED, MED), str(GRAPH_DIR / MED_NAME))
     if not os.path.exists(str(GRAPH_DIR / LRG_NAME)):
         record_graphs(gen_rand_graphs(LRG, SML), str(GRAPH_DIR / LRG_NAME))
-    
+
     graphs = load_graphs(args.mode, args.n)
-    
+
     # ground truth graphs
     log('Calculating ground truth...')
     start = time.time()
     gt_graphs = {graph : gp.chromatic_number(graph) for graph in graphs}
     log(f'{round(time.time() - start, 3)} seconds')
-    
+
     # color each graph with each algorithm
     # each algorithm will predict the chi of the graph and this will form new
     # mappings of graph -> chi
@@ -149,21 +159,21 @@ def main():
     start = time.time()
     gr_graphs = {graph: greedy_color(graph) for graph in graphs}
     log(f'{round(time.time() - start, 3)} seconds')
-    
+
     log('Calculating differential evolution colorings...')
     start = time.time()
-    de_graphs = de.compute_chi(graphs)
+    de_graphs = compute_chi(de, graphs)
     log(f'{round(time.time() - start, 3)} seconds')
     # pso.compute_chi(graphs)
     # aco.compute_chi(graphs)
-    
+
     # analyze the difference between the predictions and the actual
     table = tab.tabulate(
-        zip(list(range(len(graphs))), gt_graphs.values(), gr_graphs.values(), de_graphs.values()), 
+        zip(list(range(len(graphs))), gt_graphs.values(), gr_graphs.values(), de_graphs.values()),
         headers=['num', 'truth', 'greedy', 'de'] )
     log(f'\n{table}')
     # TODO
-    
+
     log('...finished.')
     return 0
 
---
+++ solution.py
@@ -0,0 +1,83 @@
+# author: Paul Galatic
+#
+# Graph heuristic study -- compute chi numbers
+#
+# inspired by https://ieeexplore.ieee.org/abstract/document/5952075
+
+# standard lib
+import sys
+import math
+import copy
+import random
+import operator
+
+# required lib
+import numpy as np
+
+# project lib
+from greedy_color import greedy_color
+
+class Solution:
+
+    def __init__(self, graph, omega, phi):
+        self.omega = omega
+        self.phi = phi
+        self.graph = graph
+        self.position = np.random.rand(len(self.graph))
+        self.fitness = self.fit_func()
+
+    def update(self, peers):
+        x1 = peers[0].position
+        x2 = peers[1].position
+        x3 = peers[2].position
+        # multiply x_diff by the mutation factor (omega) and add to x1
+        donor = np.zeros(len(self.position))
+        for idx in range(len(self.position)):
+            donor[idx] = x1[idx] + (self.omega * (x2[idx] - x3[idx]))
+        idy = random.randrange(len(self.position))
+        # apply crossover
+        trial = np.zeros(len(self.position))
+        for idx in range(len(self.position)):
+            chance = random.random()
+            if chance <= self.phi or idx == idy:
+                trial[idx] = donor[idx]
+            else:
+                trial[idx] = self.position[idx]
+        trial_fitness = self.fit_func(trial)
+        if trial_fitness > self.fitness:
+            self.position = trial
+            self.check_position()
+            self.fitness = self.fit_func()
+
+    def get_order(self, position=None):
+        '''
+        The ordering is found by "collapsing" the position, which is given as a
+        set of floats. The largest float is chosen first; ties are broken by
+        taking the first index available (according to np.argmax()).
+        '''
+        if type(position) == type(None): position = self.position
+        order = np.zeros(len(position))
+        pos_copy = np.array(position) # don't want to alter self.position
+        for idx in range(len(pos_copy)):
+            maxi = np.argmax(pos_copy)
+            order[idx] = maxi
+            pos_copy[maxi] = -1 # set outside the domain
+        return order
+
+    def fit_func(self, position=None):
+        '''
+        Take the ratio of the minimum possible colors used to the colors found
+        via greedy coloring + the particle's ordering. The "- 2" is to account
+        for how a graph with > 1 node requires at least two colors (and we are
+        not interested in graphs with 1 node.
+
+        Returns a value of 0 if greedy_color() == len(graph) and 1 if
+        greedy_color() == 2.
+        '''
+        order = self.get_order(position)
+        return 1 - ((greedy_color(self.graph, order) - 2) / (len(self.graph) - 2))
+
+    def check_position(self):
+        '''Ensure the values of position stay within the domain [0-1].'''
+        self.position[self.position > 1] = 1
+        self.position[self.position < 0] = 0
