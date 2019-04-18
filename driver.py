e5c88f4a07331e7d45daba82630cb76a9b26a070
Ari Zerner <ariel@zerner.com>
2019-04-18 14:08:54 -0400

    Bug fix

--- driver.py
+++ driver.py
@@ -55,7 +55,7 @@ def parse_args():
 def gen_rand_graphs(dim, theta=0.85):
     '''Generates random graphs based on parameter theta'''
     graphs = []
-    
+
     log(f'generating {NUM} Erdős-Rényi graphs of size {dim}...')
     for _ in range(NUM):
         # create a blank adjacency matrix
@@ -151,10 +151,10 @@ def main():
     graphs = load_graphs(args.mode, args.n)
 
     # ground truth graphs
-    
+
     start = time.time()
     if args.gtskip:
-        gt_graphs = {graph: None for graph in graphs} 
+        gt_graphs = {graph: None for graph in graphs}
     else:
         log('Calculating ground truth...')
         gt_graphs = {graph : gp.chromatic_number(graph) for graph in graphs}
@@ -180,11 +180,11 @@ def main():
 
     # analyze the difference between the predictions and the actual
     table_1 = tab.tabulate(
-        zip(list(range(len(graphs))), gt_graphs.values(), gr_graphs.values(), de_graphs.values(), pso_graphs.values()), 
+        zip(list(range(len(graphs))), gt_graphs.values(), gr_graphs.values(), de_graphs.values(), pso_graphs.values()),
         headers=['num', 'truth', 'greedy', 'de', 'pso'] )
     # log(f'\nChromatic numbers for graphs:\n{table_1}')
-    min_chi = min([min(gr_graphs.values()), min(de_graphs.values())])
-    max_chi = max([max(gr_graphs.values()), max(de_graphs.values())])
+    min_chi = min([min(gr_graphs.values()), min(de_graphs.values()), min(pso_graphs.values())])
+    max_chi = max([max(gr_graphs.values()), max(de_graphs.values()), max(pso_graphs.values())])
     gr_modes = [list(gr_graphs.values()).count(idx) for idx in range(min_chi, max_chi + 1)]
     de_modes = [list(de_graphs.values()).count(idx) for idx in range(min_chi, max_chi + 1)]
     pso_modes = [list(pso_graphs.values()).count(idx) for idx in range(min_chi, max_chi + 1)]
